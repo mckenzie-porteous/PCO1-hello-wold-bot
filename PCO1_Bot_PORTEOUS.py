881a642da41d64e591b1244de0215090e0a713ae
mckenzie-porteous <70336428+mckenzie-porteous@users.noreply.github.com>
2020-09-01 15:36:26 -0600

    Add files via upload

---
+++ PCO1_Bot_PORTEOUS.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 10:08:26 2020
+
+@author: mckenzieporteous
+"""
+
+
+name = input("what is your name? ")
+print(name)
+print("hello, " + name + ", nice to meet you")
+
+age = input("how old are you? ")
+print("you are " + age + " years old")
+
+family = input("how many sibillings do you have? ")
+print("you have " + family + " sibillings")
+
